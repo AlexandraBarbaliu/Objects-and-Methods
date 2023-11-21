37296981435f4d85bdb53dcbce3d60def2ca5f6e
AlexandraBarbaliu <144890724+AlexandraBarbaliu@users.noreply.github.com>
2023-11-21 20:02:57 +0200

    calculator simplu

--- scripts.js
+++ scripts.js
@@ -2,12 +2,25 @@
 
 function createPerson(name, age, city) {
     return {
-        name: Andrei,
-        age: 20,
-        city: Timisoara,
+        name: name,
+        age: age,
+        city: city,
         salutation: function () { 
-        console.log("Hello, I am " + this.name + ", I have " + this.age + "years old");
+        console.log("Hello, I am " + this.name + ", I have " + this.age + "years old", ",I am from" + this.city);
         }
-    }
+    };
 }
-var person = createPerson("Andrei", 20);
+var person = createPerson("Andrei", 20, "Timisoara");
+
+//----------------------------------------------------------------------------------------------------------------
+
+// Creează un obiect numit calculator cu o metodă numită sum care primește doi parametri și returnează suma lor.
+
+var num1 = window.prompt("Enter a number");
+var num2 = window.prompt("Enter another number");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+document.write(num1 + num2);
+
